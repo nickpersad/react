fb8ad4cdf1c55d320f75110954f8298753a9e63f
accessnick <accessnick@gmail.com>
2017-12-22 22:23:06 -0500

    More progress with discovery

--- app/scripts/components/app-main.js
+++ app/scripts/components/app-main.js
@@ -10,16 +10,13 @@ class App extends Component {
     this.state = {
       movieID: 155 // set initital load movie - The Dark Knight
     }
-    this.shows = {
-      showYear: 2017
-    }
   }
   render() {
     return (
       <div>
         <SearchBox fetchMovieID={this.fetchMovieID.bind(this)} />
         <Card data={this.state} />
-        <Showing  />
+        <Showing />
       </div>
     )
   } // END render
@@ -58,27 +55,25 @@ class App extends Component {
 
       })
     })
-  } // end function
 
-  fetchDiscoverApi(url) {
-    fetch(url).then((res) => res.json()).then((data) => {
-      this.setState({
-        shows: data
-      })
-    })
-  }
+  } // end function
 
   fetchMovieID(movieID) {
     let url = `https://api.themoviedb.org/3/movie/${movieID}?&api_key=ef77c3eda1e7e0c11a7c04c61b0d4151`
     this.fetchApi(url)
   } // end function
 
+  fetchDiscover(movieID) {
+    let url = `https://api.themoviedb.org/3/movie/${movieID}?&api_key=ef77c3eda1e7e0c11a7c04c61b0d4151`
+    this.setState({
+      shows: 'shows'
+    })
+  } // end function
+
   componentDidMount() {
     let url = `https://api.themoviedb.org/3/movie/${this.state.movieID}?&api_key=ef77c3eda1e7e0c11a7c04c61b0d4151`
-    let discoverUrl = `https://api.themoviedb.org/3/discover/movie?api_key=ef77c3eda1e7e0c11a7c04c61b0d4151&language=en-US&sort_by=popularity.desc&include_video=false&page=1&primary_release_year=2017`
 
     this.fetchApi(url)
-    this.fetchDiscoverApi(discoverUrl)
 
     //========================= BLOODHOUND ==============================//
     let suggests = new Bloodhound({
--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 const TMDBLogo = 'https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78354a4ac5abf87b2be/images/v4/logos/powered-by-rectangle-green.svg';
 
 class SearchBox extends Component {
-
   handleChange(event) {
     event.target.select();
   }
