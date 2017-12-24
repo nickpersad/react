8b7cc9ae1dc3b802df2e0c0747abf46944c4965a
accessnick <accessnick@gmail.com>
2017-12-24 10:29:29 -0500

    Center logo with search

--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -10,11 +10,10 @@ class SearchBox extends Component {
     return (
       <div className="col-xs-12 search-container nopadding">
         <div className="row">
-
-          <div className="col-xs-12 col-sm-6 col-lg-5">
+          <div className="col-xs-12 col-sm-3">
             <a href="./" title="ReactJS TMDb Movie Search"><img src={TMDBLogo} className="logo" alt="The Movie Database" /></a>
           </div>
-          <div className="col-xs-12 col-sm-6 col-lg-7">
+          <div className="col-xs-12 col-sm-6">
             <form className="searchbox">
               {/* <label> */}
                 <input ref="search suggestion" onClick={this.handleChange} className="searchbox__input typeahead form-control" type="text" placeholder="Search Movie Title..." id="q" />
