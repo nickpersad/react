89e0432e2b134cb8051504cea20973296f124dd9
accessnick <accessnick@gmail.com>
2017-12-24 17:21:26 -0500

    Add google charts to dashboard.

--- app/scripts/components/Dashboard.js
+++ app/scripts/components/Dashboard.js
@@ -1,19 +1,57 @@
 import React, { Component } from "react";
+import { Chart } from 'react-google-charts';
 
 class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: {
+        title: 'Population of Largest U.S. Cities',
+        chartArea: {width: '50%'},
+        hAxis: {
+          title: 'Total Population',
+          minValue: 0
+        },
+        vAxis: {
+          title: 'City'
+        }
+      },
+      data: [
+        ['City', '2010 Population',],
+        ['New York City, NY', 8175000],
+        ['Los Angeles, CA', 3792000],
+        ['Chicago, IL', 2695000],
+        ['Houston, TX', 2099000],
+        ['Philadelphia, PA', 1526000],
+      ],
+    };
+  }
   render() {
     return (
-      <div>
-        <h2>Dashboard</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
+      <div className="col-xs-12 dashboardcont nopadding">
 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-      </div>
+          <div className="dashboard-container col-xs-12">
+            <h1>Dashboard</h1>
+            <div className="row">
+              <div className="chart-container col-xs-6">
+                <Chart
+                  chartType="BarChart"
+                  data={this.state.data}
+                  options={this.state.options}
+                  graph_id="BarChart"
+                  width="100%"
+                  height="400px"
+                  legend_toggle
+                />
+              </div>
+              <div className="chart-container col-xs-6">
+                <div className="chart">
+                  <p>chart 2</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     );
   }
 }
