4af23a4a3e4251f045ad33ea0957eaa243bf6efd
accessnick <accessnick@gmail.com>
2017-12-26 13:59:18 -0500

    Check update if props changed

--- app/scripts/components/movies/gif.js
+++ app/scripts/components/movies/gif.js
@@ -15,16 +15,7 @@ class Gif extends Component {
 
   }
 
-  componentDidUpdate() {
-    let movieName = 'The Dark Knight'
-
-    if (this.props !== undefined && this.props.data) {
-      movieName = this.props.data.original_title
-    }
-
-    movieName = movieName.replace(/\s+/g, '+').toLowerCase()
-    let url = `http://api.giphy.com/v1/gifs/search?q=${movieName}&api_key=mCBE3kU90eQny60ZkZ9VYS56uVDPhFD2&limit=1`
-
+  fetchApi(url) {
     fetch(url).then((res) => res.json()).then((data) => {
       // console.log(data['data'])
       this.setState({
@@ -34,13 +25,28 @@ class Gif extends Component {
     })
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.data !== this.props.data) {
+      let movieName = 'The Dark Knight'
+
+      if (this.props !== undefined && this.props.data) {
+        movieName = this.props.data.original_title
+      }
+
+      movieName = movieName.replace(/\s+/g, '+').toLowerCase()
+      let url = `http://api.giphy.com/v1/gifs/search?q=${movieName}&api_key=mCBE3kU90eQny60ZkZ9VYS56uVDPhFD2&limit=1`
+
+      this.fetchApi(url)
+    }
+  }
+
   render() {
     return (
       <div className="giphy-container col-xs-12">
         <div className="row">
+          <h2>Thanks for Visiting</h2>
           {this.state.gifArr.map(item =>
-            <div key={item.id} className="col-xs-12">
-              <h2 title={item.title}>Thanks for Visiting</h2>
+            <div key={item.id}>
               <iframe src={`${item.embed_url}`} frameBorder="0" className="giphy-embed"></iframe>
             </div>
           )}
