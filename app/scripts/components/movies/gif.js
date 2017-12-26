1044b967ab2d692b0505685c3bf78a3fec93faf7
accessnick <accessnick@gmail.com>
2017-12-26 12:06:34 -0500

    Update gif when movie is changed

--- app/scripts/components/movies/gif.js
+++ app/scripts/components/movies/gif.js
@@ -11,18 +11,27 @@ class Gif extends Component {
       };
 
       // the api request function
-      let url = `http://api.giphy.com/v1/gifs/search?q=deadpool&api_key=mCBE3kU90eQny60ZkZ9VYS56uVDPhFD2&limit=1`
+      // let url = `http://api.giphy.com/v1/gifs/search?q=the+dark+knight&api_key=mCBE3kU90eQny60ZkZ9VYS56uVDPhFD2&limit=1`
 
-      fetch(url).then((res) => res.json()).then((data) => {
-        console.log(data['data'])
-        this.setState({
-          gifArr: data['data'],
-          embed: data['data'][0]['embed_url']
-        })
-      })
   }
 
-  componentDidMount() {
+  componentDidUpdate() {
+    let movieName = 'The Dark Knight'
+
+    if (this.props !== undefined && this.props.data) {
+      movieName = this.props.data.original_title
+    }
+
+    movieName = movieName.replace(/\s+/g, '+').toLowerCase()
+    let url = `http://api.giphy.com/v1/gifs/search?q=${movieName}&api_key=mCBE3kU90eQny60ZkZ9VYS56uVDPhFD2&limit=1`
+
+    fetch(url).then((res) => res.json()).then((data) => {
+      // console.log(data['data'])
+      this.setState({
+        gifArr: data['data'],
+        embed: data['data'][0]['embed_url']
+      })
+    })
   }
 
   render() {
@@ -32,7 +41,7 @@ class Gif extends Component {
           {this.state.gifArr.map(item =>
             <div key={item.id} className="col-xs-12">
               <h2>{item.title}</h2>
-              <iframe src={`${item.embed_url}`} frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+              <iframe src={`${item.embed_url}`} frameBorder="0" className="giphy-embed"></iframe>
             </div>
           )}
         </div>
